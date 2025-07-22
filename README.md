1927e449da4270e13272c6bfb9018a58bda0d31c
tejpratapsingh <tej.singh@catalyzesystems.com>
2025-07-22 22:23:46 +0530

    updated gradle version

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # AndroidStepSeekBar
 Android Step SeekBar
+
+### Features
+1. Customisable step size, duh :P
+2. Customisable range
+
+### How to use
+```kotlin
+findViewById<StepSeekBar>(R.id.step_seek_bar).apply {
+            setSelectableRange(30, 70, true)
+        }.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seekBar: SeekBar?,
+                progress: Int,
+                fromUser: Boolean
+            ) {
+                Log.d(TAG, "onProgressChanged: $progress")
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                Log.d(TAG, "onStartTrackingTouch: ")
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                Log.d(TAG, "onStopTrackingTouch: ")
+            }
+
+        })
+```
--- app/src/main/java/com/tejpratapsingh/stepseekbar/MainActivity.kt
+++ app/src/main/java/com/tejpratapsingh/stepseekbar/MainActivity.kt
@@ -2,16 +2,41 @@ package com.tejpratapsingh.stepseekbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.util.Range
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import com.tejpratapsingh.libstepseekbar.StepSeekBar
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<StepSeekBar>(R.id.step_seek_bar).apply {
             setSelectableRange(30, 70, true)
-        }
+        }.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seekBar: SeekBar?,
+                progress: Int,
+                fromUser: Boolean
+            ) {
+                Log.d(TAG, "onProgressChanged: $progress")
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                Log.d(TAG, "onStartTrackingTouch: ")
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                Log.d(TAG, "onStopTrackingTouch: ")
+            }
+
+        })
     }
 }
